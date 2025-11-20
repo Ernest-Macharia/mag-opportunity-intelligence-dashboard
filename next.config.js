b2aec6ab223ab28467642bee78af4b9da100210b
Ernest Thuku Macharia <ernest.bposeats@gmail.com>
2025-11-20 14:05:34 +0300

    update json files

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
   env: {
     NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
